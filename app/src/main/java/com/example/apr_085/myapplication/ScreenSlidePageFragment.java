3f1c61c7896ec801aa2070cbd06611aecc33abb2
michaeloc <michaeloc9@gmail.com>
2017-12-06 12:29:10 -0300

    first commit

---
+++ app/src/main/java/com/example/apr_085/myapplication/ScreenSlidePageFragment.java
@@ -0,0 +1,50 @@
+package com.example.apr_085.myapplication;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by apr-085 on 07/11/17.
+ */
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    public final String TAG = ScreenSlidePageFragment.class.getName().toString();
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragrment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.i(TAG,"onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+}
